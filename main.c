9ba33b27c15a2d0a51adda56e9addba82ca24421
Michael Sanders <michaelsand155@gmail.com>
2023-08-03 12:48:05 +1000

    Update main.c
    made void dataparser more efficient by including string.h library

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 struct data {
 	char wholeInput[100];
 	char firstWord[100];
-	char remainder[100];
+	char *remainder;
 };
 
 void calc() {
@@ -32,32 +33,10 @@ void get() {
 }
 
 
+void dataParser(struct data* userInput){
 
-void dataParser(struct data* userInput) {
-
-
-	if (strchr(userInput->wholeInput, ' ') != NULL) { //detects if line has a space or now [one or (two or more) words]
-		for (int i = 0, j = 0; i < strlen(userInput->wholeInput); i++, j++) { // splits string into first word, and remainder
-			if (userInput->wholeInput[i] != ' ') {
-				userInput->firstWord[j] = userInput->wholeInput[i]; //first word iteration
-			}
-			else {
-				userInput->firstWord[j] = '\n';
-				userInput->firstWord[j + 1] = '\0';
-				i++;
-				for (j = 0; i < strlen(userInput->wholeInput); i++, j++) {
-					userInput->remainder[j] = userInput->wholeInput[i]; //remainder iteration
-				}
-				userInput->remainder[j] = '\n';
-				userInput->remainder[j + 1] = '\0';
-				break;
-			}
-		}
-	}
-	else {
-		strcpy(userInput->firstWord, userInput->wholeInput); //if one word, strcpy
-		userInput->remainder[0] = '\0'; // null end the string so strlen = 0
-	}
+	strcpy_s(userInput->firstWord, sizeof(userInput->firstWord), userInput->wholeInput);
+	strtok_s(userInput->firstWord, " \n", &userInput->remainder);
 
 	return;
 }
@@ -65,37 +44,37 @@ void dataParser(struct data* userInput) {
 
 void functionTable(struct data* userInput) {
 
-	if (!(strcmp(userInput->firstWord, "calc\n"))) {
-		if (strlen(userInput->remainder) == 0) { //checks that a remainder was parsed
+	if (!(strcmp(userInput->firstWord, "calc\0"))) {
+		if (!(strcmp(userInput->remainder, ""))) { //checks that a remainder was parsed
 			printf("Command \"calc\" needs an expression\n\"calc + 3 4 3\"\n");
 			return;
 		}
 		calc();
 	}
-	else if (!(strcmp(userInput->firstWord, "time\n"))) {
+	else if (!(strcmp(userInput->firstWord, "time\0"))) {
 		shellTime();
 	}
-	else if (!(strcmp(userInput->firstWord, "path\n"))) {
+	else if (!(strcmp(userInput->firstWord, "path\0"))) {
 		path();
 	}
-	else if (!(strcmp(userInput->firstWord, "sys\n"))) {
+	else if (!(strcmp(userInput->firstWord, "sys\0"))) {
 		sys();
 	}
-	else if (!(strcmp(userInput->firstWord, "put\n"))) {
-		if (strlen(userInput->remainder) == 0) {
+	else if (!(strcmp(userInput->firstWord, "put\0"))) {
+		if (userInput->remainder == NULL) {
 			printf("Command \"put\" needs a directory name and filename\n\"put file.txt folder\"\n");
 			return;
 		}
 		put();
 	}
-	else if (!(strcmp(userInput->firstWord, "get\n"))) {
-		if (strlen(userInput->remainder) == 0) {
+	else if (!(strcmp(userInput->firstWord, "get\0"))) {
+		if (userInput->remainder == NULL) {
 			printf("Command \"get\" needs a filename\n\"get file.txt\"\n");
 			return;
 		}
 		get();
 	}
-	else if (!(strcmp(userInput->firstWord, "quit\n"))) {
+	else if (!(strcmp(userInput->firstWord, "quit\0"))) {
 		return;
 	}
 	else {
@@ -106,19 +85,10 @@ void functionTable(struct data* userInput) {
 }
 
 
-void test(struct data* userInput) {
-
-	printf("%s", userInput->wholeInput);
-
-	fgets(userInput->remainder, sizeof(userInput->remainder), stdin);
-	return;
-
-}
-
 
 int main(void) {
 
-	struct data userInput = (struct data){ .firstWord[0] = '\0', .wholeInput[0] = '\0', .remainder[0] = '\0' };
+	struct data userInput = (struct data){ .firstWord[0] = '\0', .wholeInput[0] = '\0', .remainder = NULL};
 
 	do {
 		printf("$ ");
@@ -129,7 +99,7 @@ int main(void) {
 
 		functionTable(&userInput);
 
-	} while (strcmp(userInput.firstWord, "quit\n"));
+	} while (strcmp(userInput.firstWord, "quit\0"));
 
 
 
